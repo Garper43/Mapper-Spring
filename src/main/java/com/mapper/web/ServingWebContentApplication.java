8bec86c3fd27877c036a5d5428ebec4699727f3e
Garper43 <tarassozansky@gmail.com>
2023-02-03 14:10:16 -0800

    mde it run

--- src/main/java/com/mapper/web/ServingWebContentApplication.java
+++ src/main/java/com/mapper/web/ServingWebContentApplication.java
@@ -23,12 +23,4 @@ public class ServingWebContentApplication {
 
 		app.run(args);
 	}
-
-//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//		http.
-//			formLogin(form -> form
-//				.loginPage("/login")
-//				.permitAll()
-//		);
-//	}
 }
